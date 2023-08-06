eb9d2293b9e8ec0ee2ca0a20586f708508ae7165
류선영 <syryu@imgr.co.kr>
2023-08-07 08:53:27 +0900

    fix : save table

--- jdbc_to_file.py
+++ jdbc_to_file.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp
 
 mysql_ip = "ryu-mysql.c9iezgjnbwtd.ap-northeast-2.rds.amazonaws.com"
 mysql_port = "3306"
@@ -12,7 +13,7 @@ mysql_table_emp_dept = "dept_emp"
 mysql_table_dept = "departments"
 
 write_db = "examples"
-write_table = "emp_work_years"
+write_table = "emp_work"
 
 spark = SparkSession.builder\
         .appName("mysql_make_datamart")\
@@ -50,54 +51,6 @@ emp_dept_df.createOrReplaceTempView("dept_emp")
 dept_df.createOrReplaceTempView("departments")
 
 
-# spark.sql("""
-# select deyear.emp_no as emp_no
-#                 , deyear.dept_no as dept_no
-#                 , d.dept_name as dept_name
-#                 , deyear.to_date as to_date
-#                 , deyear.from_date as from_date
-#                 , DATEDIFF(case when max(to_date)='9999-01-01' 
-# 					then current_timestamp() 
-# 					else to_date
-# 					end, min(from_date))/365 as dept_years 
-# 			from dept_emp deyear
-# 	        left join departments d on deyear.dept_no = d.dept_no 
-# 			group by deyear.emp_no, d.dept_name, deyear.dept_no, deyear.to_date, deyear.from_date
-# """).show()
-
-
-# spark.sql("""
-# select * 
-#     , DATEDIFF(case when to_date='9999-01-01' 
-# 					then current_timestamp() 
-# 					else to_date
-# 					end,
-# 					from_date
-# 					) / 365 as work_years
-# from (
-#     select deyear.emp_no 
-# 		, array_join(collect_list(deyear.dept_no), ', ') as dept_no
-# 		, array_join(collect_list(deyear.dept_name), ', ') as dept_name
-# 		, array_join(collect_list(deyear.dept_years), ', ') as dept_work_years
-#         , max(deyear.to_date) as to_date
-#         , min(deyear.from_date) as from_date
-# 	from (select de.emp_no as emp_no
-#                 , de.dept_no as dept_no
-#                 , d.dept_name as dept_name
-#                 , de.to_date as to_date
-#                 , de.from_date as from_date
-#                 , DATEDIFF(case when max(de.to_date)='9999-01-01' 
-# 					then current_timestamp() 
-# 					else de.to_date
-# 					end, min(de.from_date))/365 as dept_years 
-# 			from dept_emp de
-# 	        left join departments d on de.dept_no = d.dept_no 
-# 			group by de.emp_no, d.dept_name, de.dept_no, de.to_date, de.from_date
-#         ) deyear
-#     group by deyear.emp_no
-# )
-# """).show()
-
 new_df = spark.sql("""
 select e.emp_no, e.first_name, e.last_name, e.gender, de.dept_no, de.dept_name, de.dept_work_years, de.work_years
 from employees e 
@@ -134,14 +87,18 @@ left join (
 ) de on e.emp_no = de.emp_no
 """)
 
+
+new_df = new_df.withColumn("create_time", current_timestamp().cast("timestamp"))
+
 new_df.printSchema()
 new_df.show()
 
-# new_df.write \
-#     .format("jdbc")\
-#     .option("url","jdbc:mysql://{0}:{1}/{2}".format( mysql_ip, mysql_port, write_db ) )\
-#     .option("driver", mysql_driver)\
-#     .option("dbtable", write_table)\
-#     .option("user", user)\
-#     .option("password", password)\
-#     .save()
+new_df.write \
+    .mode("overwrite")\
+    .format("jdbc")\
+    .option("url","jdbc:mysql://{0}:{1}/{2}".format( mysql_ip, mysql_port, write_db ) )\
+    .option("driver", mysql_driver)\
+    .option("dbtable", write_table)\
+    .option("user", user)\
+    .option("password", password)\
+    .save()
